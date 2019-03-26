0f2db2f33bb01cae122246fb0931740135077b9a
Sergey Kozlov <sergey.kozlov@hotmail.com>
2019-03-26 16:32:44 +0200

    Add project files.

---
+++ SimpleFeedReader/Startup.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleFeedReader.Services;
+
+namespace SimpleFeedReader
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<NewsService>();
+            services.AddAutoMapper();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+              
+            }
+
+            // Set security headers
+            app.UseXContentTypeOptions();
+            app.UseXfo(options => options.SameOrigin());
+            app.UseXXssProtection(options => options.EnabledWithBlockMode());
+            app.UseReferrerPolicy(options => options.StrictOrigin());
+            app.UseHsts(hsts => hsts.MaxAge(365).IncludeSubdomains());
+
+            app.UseCsp(options => options
+                .BlockAllMixedContent()
+                .StyleSources(s => s.Self())
+                .StyleSources(s => s.UnsafeInline())
+                .FontSources(s => s.Self())
+                .FormActions(s => s.Self())
+                .FrameAncestors(s => s.Self())
+                .ImageSources(s => s.Self())
+                .ScriptSources(s => s.Self())
+            );
+            
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
