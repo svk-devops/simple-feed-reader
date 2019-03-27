5c190160a9be428ff4aa4e5efecb68d5a723fde9
svk-devops <48982833+svk-devops@users.noreply.github.com>
2019-03-27 15:23:52 +0200

    Update Startup.cs
    options.CheckConsentNeeded = context => true;

--- SimpleFeedReader/Startup.cs
+++ SimpleFeedReader/Startup.cs
@@ -21,6 +21,13 @@ namespace SimpleFeedReader
         {
             services.AddScoped<NewsService>();
             services.AddAutoMapper();
+            
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
